f0692b2a24f33eb60c57af87c2c5620955f172ac
exsystem <exsystemchina@gmail.com>
2014-11-04 06:48:17 +0000

    [FrameworkDSW\Configuration\Json] * 新增JSON格式配置文件讀寫引擎

--- FrameworkDSW/Configuration_Json.php
+++ FrameworkDSW/Configuration_Json.php
@@ -1,22 +1,24 @@
 <?php
 /**
- * \FrameworkDSW\Configuration\PhpArray
+ * \FrameworkDSW\Configuration\Json
  * @author  许子健
  * @version $Id$
  * @since   separate file since reversion 91
  */
-namespace FrameworkDSW\Configuration\PhpArray;
+namespace FrameworkDSW\Configuration\Json;
 
 use FrameworkDSW\Configuration\IConfigurationStorage;
 use FrameworkDSW\Containers\TMap;
 use FrameworkDSW\Framework\Framework;
 use FrameworkDSW\System\EException;
+use FrameworkDSW\System\ERuntimeException;
 use FrameworkDSW\System\TObject;
 use FrameworkDSW\Utilities\TType;
 
+//FIXME exception type
 /**
  * Class TJsonStorage
- * @package FrameworkDSW\Configuration\PhpArray
+ * @package FrameworkDSW\Configuration\Json
  */
 class TJsonStorage extends TObject implements IConfigurationStorage {
     /**
@@ -30,7 +32,7 @@ class TJsonStorage extends TObject implements IConfigurationStorage {
     public function __construct($FileName) {
         parent::__construct();
         TType::String($FileName);
-        $this->FFileName  =$FileName;
+        $this->FFileName = $FileName;
     }
 
     /**
@@ -43,20 +45,71 @@ class TJsonStorage extends TObject implements IConfigurationStorage {
             throw new EException(sprintf('Unable read file: "%s".', $this->FFileName));
         }
         else {
-            $mArr = json_decode($mContent, true);
-            if (is_array($mArr) === false) {
+            $mArray = json_decode($mContent, true);
+            if (is_array($mArray) && array_diff_key($mArray, array_keys(array_keys($mArray))) === false) {
                 throw new EException(sprintf('Wrong structure: "%s".', $this->FFileName));
             }
-            TMap::PrepareGeneric(['K' => Framework::String, 'V'=>Framework::Variant]);
+
+            $mStack = [];
+
+            TMap::PrepareGeneric(['K' => Framework::String, 'V' => Framework::Variant]);
             $mResult = new TMap();
+
+            /** @var string $mKey */
+            foreach ($mArray as $mKey => &$mValue) {
+                if (is_array($mValue) && array_diff_key($mValue, array_keys(array_keys($mValue)))) {
+                    $mStack[] = [$mKey, $mValue];
+                }
+                else {
+                    $mResult->Put($mKey, $mValue);
+                }
+            }
+
+            while (count($mStack) > 0) {
+                $mCurrentPair = array_pop($mStack);
+                /** @var string $mCurrentPath */
+                $mCurrentPath = $mCurrentPair[0];
+                $mCurrentData = $mCurrentPair[1];
+                /** @var string $mKey */
+                foreach ($mCurrentData as $mKey => &$mValue) {
+                    if (is_array($mValue) && array_diff_key($mValue, array_keys(array_keys($mValue)))) {
+                        $mStack[] = ["{$mCurrentPath}.{$mKey}", $mValue];
+                    }
+                    else {
+                        $mResult->Put("{$mCurrentPath}.{$mKey}", $mValue);
+                    }
+                }
+            }
+
             return $mResult;
         }
     }
 
     /**
-     * @param \FrameworkDSW\Containers\TMap <K: string, V: mixed> $Config
+     * @param  \FrameworkDSW\Containers\TMap <K: string, V: mixed> $Config
+     * @throws EException
+     * @throws \FrameworkDSW\Utilities\EInvalidObjectCasting
      */
     public function Flush($Config) {
-        // TODO: Implement Flush() method.
+        TType::Object($Config, [TMap::class => ['K' => Framework::String, 'V' => Framework::Variant]]);
+
+        try {
+            $mTempArray = [];
+            $mTemp      = &$mTempArray;
+            foreach ($Config as $mKey => $mValue) {
+                $mKeys = explode('.', $mKey);
+                foreach ($mKeys as $k) {
+                    $mTemp = &$mTemp[$k];
+                }
+                $mTemp = $mValue;
+                $mTemp = &$mTempArray;
+            }
+
+            $mResult = json_encode($mTempArray);
+            file_put_contents($this->FFileName, $mResult);
+        }
+        catch (ERuntimeException $Ex) {
+            throw new EException('Failed to set file contents.');
+        }
     }
 }
